6c7e64fb09e2c3f5cba52f8b7f5c67539179f78f
Pawel.Flis <pawel.flis@comarch.pl>
2022-05-25 11:03:11 +0200

    XYZ-000 Add offer accept

--- src/main/java/com/smalaca/sale/domain/cart/Cart.java
+++ src/main/java/com/smalaca/sale/domain/cart/Cart.java
@@ -10,21 +10,46 @@ public class Cart {
     private final List<CartItem> items = new ArrayList<>();
 
     public void addProduct(Warehouse warehouse, ProductId productId, Amount amount) {
-        Optional<Assortment> assortment = warehouse.findAssortmentFor(productId);
+      checkProductAvailable(warehouse, productId, amount);
 
-        if (assortment.isEmpty()) {
-            throw AssortmentException.doesNotExist(productId);
-        }
-
-        if (assortment.get().hasNotEnough(amount)) {
-            throw AssortmentException.hasNotEnoughProducts(productId, amount);
-        }
-
-        items.add(new CartItem(productId, amount));
+      items.add(new CartItem(productId, amount));
     }
 
-    public Offer accept(Buyer buyer, List<ProductId> productIds) {
+  private void checkProductAvailable(Warehouse warehouse, ProductId productId, Amount amount) {
+    Optional<Assortment> assortment = warehouse.findAssortmentFor(productId);
 
+    if (assortment.isEmpty()) {
+        throw AssortmentException.doesNotExist(productId);
+    }
+
+    if (assortment.get().hasNotEnough(amount)) {
+        throw AssortmentException.hasNotEnoughProducts(productId, amount);
+    }
+  }
+
+  private void checkProductsAvailable(Warehouse warehouse, List<ProductId> productIds, Amount amount) {
+    Optional<Assortment> assortment = warehouse.findAssortmentFor(productId);
+
+    if (assortment.isEmpty()) {
+        throw AssortmentException.doesNotExist(productId);
+    }
+
+    if (assortment.get().hasNotEnough(amount)) {
+        throw AssortmentException.hasNotEnoughProducts(productId, amount);
+    }
+  }
+
+  public Offer accept(Buyer buyer, List<ProductId> productIds) {
+      if (hasNotAll(productIds)) {
+        throw CartException.hasNotAll(productIds);
+      }
+
+      checkProductAvailable(warehouse, );
+      
       return null;
     }
+
+  private boolean hasNotAll(List<ProductId> productIds) {
+    return false;
+  }
 }
